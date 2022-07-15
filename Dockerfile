289536448e27d1c2aca0aaf272ec1d6be49d84c0
Mattias Rylander <mattias.rylander@gmail.com>
2022-07-15 13:30:41 +0200

    Back to Ubuntu, Snyk vulnerability scanning

--- Dockerfile
+++ Dockerfile
@@ -12,11 +12,15 @@
 
 # Ubuntu
 FROM ubuntu:latest
+=======
+# Ubuntu
+FROM ubuntu:20.04
+>>>>>>> Stashed changes
 RUN apt-get update \
   && apt-get install -y openssh-client open-vm-tools \
   && rm -rf /var/lib/apt/lists/*
 
-COPY run-debian.sh /run.sh
+COPY run.sh /run.sh
 COPY shutdown.sh /sbin/shutdown
 RUN command
 CMD [ "/run.sh" ]
--- build&push.sh
+++ build&push.sh
@@ -7,13 +7,29 @@ fi
 
 echo "Building emryl/docker-synology-open-vm-tools:$1 docker image"
 
-read -r -p "Are you sure to run this script ? [Y/n] " answer
+read -r -p "Build Dockerfile ? [y/N] " answer
 if [ "$answer" != "${answer#[Yy]}" ] ;then
 	docker build --no-cache --pull -t emryl/docker-synology-open-vm-tools:$1 .
+	#docker push emryl/docker-synology-open-vm-tools:$1
+	echo "Successfully built emryl/docker-synology-open-vm-tools:$1"
+else
+	echo "Did not build a new version"
+fi
+
+read -r -p "Run Docker Scan with Snyk ? [y/N] " answer
+if [ "$answer" != "${answer#[Yy]}" ] ;then
+	docker scan --file ./Dockerfile emryl/docker-synology-open-vm-tools:$1
+	echo "Successfully scanned emryl/docker-synology-open-vm-tools:$1"
+else
+	echo "Did not scan for vulnerabilities"
+fi
+
+read -r -p "Push to DockerHub ? [y/N] " answer
+if [ "$answer" != "${answer#[Yy]}" ] ;then
 	docker push emryl/docker-synology-open-vm-tools:$1
 	echo "Successfully pushed emryl/docker-synology-open-vm-tools:$1"
-	exit 0
 else
-	echo "Abort"
-	exit 1
+	echo "Finished without pushing to DockerHub"
 fi
+
+exit 0
